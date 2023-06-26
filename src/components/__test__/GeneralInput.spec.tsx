6466d4379a3a77d5c92ecef7acb20dd227af355a
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-06-26 14:40:03 +0300

    Implement test cases for GeneralInput component

---
+++ src/components/__test__/GeneralInput.spec.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import GeneralInput from '../GeneralInput';
+
+describe('GeneralInput', () => {
+  it('renders correctly', () => {
+    const handleChange = jest.fn();
+    render(
+      <GeneralInput onValueChange={handleChange} value="" placeHolder="" />
+    );
+  });
+
+  it('input value changes correctly', () => {
+    const handleChange = jest.fn();
+    render(
+      <GeneralInput
+        onValueChange={handleChange}
+        value="John"
+        placeHolder="E.g John"
+      />
+    );
+
+    const input = screen.getByTestId('general-input') as HTMLInputElement;
+    fireEvent.change(input, {
+      target: { value: 'Doe' },
+    });
+    expect(input.value).toBe('Doe');
+  });
+
+  it('contains the right placeholder value', () => {
+    const handleChange = jest.fn();
+    render(
+      <GeneralInput
+        onValueChange={handleChange}
+        value="John"
+        placeHolder="E.g John"
+      />
+    );
+
+    const input = screen.getByTestId('general-input') as HTMLInputElement;
+    expect(input.placeholder).toBe('E.g John');
+  });
+});
