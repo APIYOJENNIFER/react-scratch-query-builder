d36789dd9f3cebad84758e99f0fe42940ec3f023
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-06-30 12:23:19 +0300

    Add configurations to enable testing for TypeScript files and fix errors

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!nanoid)'],
+  moduleNameMapper: {
+    nanoid: 'nanoid',
+  },
+};
